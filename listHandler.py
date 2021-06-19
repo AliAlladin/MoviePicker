7f5733f3d56c39fdf16d2290a8281bf2abbd8f9a
Ali Alladin <ali.alladin20@gmail.com>
2021-06-19 16:24:06 +0200

    Moved out json functionality to separate file. Movies can now also be added to watchlist.

--- listHandler.py
+++ listHandler.py
@@ -9,3 +9,28 @@ def addToJson(filename, movie):
 
     with open('./' + filename, 'w') as json_file:
         json.dump(data, json_file, indent=2)
+
+# Function deletes a movie from json file provided the name of the file and the movie json.
+def deleteFromJson(filename, movie):
+    with open('./' + filename) as json_file:
+        data = json.load(json_file)
+
+    temp = data['movies']
+    for i in range(len(temp)):
+        if temp[i]['id'] == movie['id']:
+            del temp[i]
+            break
+
+    with open('./' + filename, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
+
+def alreadyInList(filename, movie):
+    with open('./' + filename) as json_file:
+        data = json.load(json_file)
+
+    temp = data['movies']
+    for i in range(len(temp)):
+        if temp[i]['id'] == movie['id']:
+            return True
+
+    return False
--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def printMenu():
 def getChoice():
     choice = input('Type a number and press enter: ')
     print()
+
     if choice == '1':
         title = input("Type the title of the movie and press enter: ")
         movies = api.searchMovie(title)
         for movie in movies:
             print(movie)
-
     elif choice == '2':
         findSomethingGood()
     elif choice == '3':
@@ -34,23 +34,32 @@ def getChoice():
         print('\nPlease choose one of the alternatives in the menu: ')
         getChoice()
 
-# Randomly returns a movie among the top250
+# Recommends the user random movies from the top 250.
 def findSomethingGood():
     # top250Movies = api.getTop250();
-
     with open('./top250.json') as top250:
         top250Movies = json.loads(top250.read())
 
     while(True):
         # movie = random.choice(top250Movies)
         movie = random.choice(top250Movies['items'])
-        print(movie)
+        if not alreadyInList('seen.json', movie) and not alreadyInList('watchlist.json', movie):
+            print(movie)
+        else:
+            findSomethingGood()
 
         choice = input('Already seen this movie? (yes/no): ')
         if choice == 'yes':
             addToJson('seen.json', movie)
             continue
         elif choice == 'no':
+            choice = input('Want add this to your watchlist? (yes/no): ')
+            if choice == 'yes':
+                addToJson('watchlist.json', movie)
+                print('Added to your watchlist!\n')
+            elif choice == 'no':
+                continue
+
             choice = input('Want another recommendation? (yes/no): ')
             if choice == 'yes':
                 continue
